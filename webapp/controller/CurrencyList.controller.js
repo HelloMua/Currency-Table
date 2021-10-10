e84439b849aecd9b3fd333982073eabf7e0f27e8
HelloMua <jmua333@gmail.com>
2021-10-10 10:32:25 +0000

    Input Data to Table Model

--- webapp/controller/CurrencyList.controller.js
+++ webapp/controller/CurrencyList.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/core/mvc/XMLView"
-], function (controller, JSONModel, XMLView) {
+    "sap/ui/core/mvc/XMLView",
+    "sap/ui/core/format/NumberFormat"
+], function (controller, JSONModel, XMLView, NumberFormat) {
     "use strict";
 
     return controller.extend("sap.ui.demo.walkthrough.CurrencyList", {
@@ -34,17 +35,15 @@ sap.ui.define([
             ]);
             this.getView().setModel(oTableModel, "tableData");
 
-            var oInputModel = new JSONModel([
-                {
-                    usdInput  : null
-                },
-                {
-                    euroInput : null
-                },
-                {
+            var oTableDataModel = this.getView().getModel("tableData").getProperty("/");
+
+            var oInputModel = new JSONModel({
+                    krwInput  : null,
+                    usdInput  : null,
+                    euroInput : null,
                     jpyInput  : null
                 }
-            ]);
+            );
 
             this.getView().setModel(oInputModel, "inputCurrency");
         },
@@ -71,7 +70,7 @@ sap.ui.define([
                 amountInputField2.setValueStateText("값을 입력해 주세요.");
                 amountInputField.focus();
             } else {
-                amountInputField2.setValueState("None");
+                amountInputField2.setValueState("Success");
                 amountInputField2.setValueStateText("");
             }
 
@@ -81,9 +80,9 @@ sap.ui.define([
             if (!amountInputField3.getValue()) {
                 amountInputField3.setValueState("Error");
                 amountInputField3.setValueStateText("값을 입력해 주세요.");
-                // amountInputField3.focus();
+                amountInputField3.focus();
             } else {
-                amountInputField3.setValueState("None");
+                amountInputField3.setValueState("Success");
                 amountInputField3.setValueStateText("");
             }
 
@@ -93,31 +92,32 @@ sap.ui.define([
             if (!amountInputField4.getValue()) {
                 amountInputField4.setValueState("Error");
                 amountInputField4.setValueStateText("값을 입력해 주세요.");
-                // amountInputField4.focus();
+                amountInputField4.focus();
             } else {
-                amountInputField4.setValueState("None");
+                amountInputField4.setValueState("Success");
                 amountInputField4.setValueStateText("");
             }
 
             // getProperty의 쓰임새??
             var oTableDataModel = this.getView().getModel("tableData").getProperty("/");
-            console.log(oTableDataModel);
 
-            // oTableDataModel[0].rate = "1,193.60";
-            // oTableDataModel[1].rate = "1,378.50";
-            // oTableDataModel[2].rate = "1,066.71";
+            oTableDataModel[0].rate = amountInputField2.getValue();
+            oTableDataModel[1].rate = amountInputField3.getValue();
+            oTableDataModel[2].rate = amountInputField4.getValue();
 
-            // var intArray = [];
-
-            oTableDataModel[0].rate = amountInputField2.setValue(amountInputField2.getValue());
-            oTableDataModel[1].rate = amountInputField3.setValue(amountInputField3.getValue());
-            oTableDataModel[2].rate = amountInputField4.setValue(amountInputField4.getValue());
+            var sParseFloatPrice = parseFloat(amountInputField.getValue());
+            // console.log(isNaN(amountInputField.getValue()));
+            console.log(isNaN(sParseFloatPrice));
 
             for (var i=0; i<oTableDataModel.length; i++) {
+                // var sParseFloatRate = parseFloat(oTableDataModel[i].rate);
+                console.log(isNaN(oTableDataModel[i].rate));
                 // js는 문자열에 숫자를 곱하면 숫자 / 숫자타입에 문자열을 더하면 문자
-                oTableDataModel[i].value = oTableDataModel[i].rate * amountInputField.getValue(); 
-                // intArray[i] = parseInt(oTableDataModel[i].rate);
+                // oTableDataModel[i].value = oTableDataModel[i].rate * amountInputField.getValue();
+                oTableDataModel[i].value = oTableDataModel[i].rate * sParseFloatPrice; 
+                
             }
+            console.log(oTableDataModel);
         },
 
         // 입력 필드의 텍스트가 변경되고 포커스가 입력 필드를 벗어나거나 Enter 키를 누르면 시작됩니다.
@@ -125,9 +125,9 @@ sap.ui.define([
             var inputItem = oEvent.getSource();
 
             // 정규식 사용
-            var regex = /[^0-9]/g;      // 0-9 : 숫자, [^문자] : 문자 제외, g: 모든 문자 검색
-            var result = inputItem.getValue().replace(regex, "");       // regex(/정규표현식/)에 매칭되는 항목을 ""(대체문자열)로 바꿈
-            inputItem.setValue(result);
+            // var regex = /[^0-9]/g;      // 0-9 : 숫자, [^문자] : 문자 제외, g: 모든 문자 검색
+            // var result = inputItem.getValue().replace(regex, "");       // regex(/정규표현식/)에 매칭되는 항목을 ""(대체문자열)로 바꿈
+            // inputItem.setValue(result);
         },
 
         // 각 키 입력, 삭제, 붙여넣기 등의 사용자 상호 작용에 의해 입력 값이 변경될 때 발생합니다.
@@ -137,6 +137,25 @@ sap.ui.define([
             // var regex = /[^0-9]/g; 
             // var result = inputItem.getValue().replace(regex, "");
             // inputItem.setValue(result);
-        }
+        },
+
+        formatNumber : function (value) {
+			// var oFloatFormatter = NumberFormat.getFloatInstance({
+			// 	style: "short",
+			// 	// decimals: 0 		// short는 정수형이므로 decomals 0은 의미가 없음
+			// });
+			// return oFloatFormatter.format(value);
+
+			var oCurrencyFormatter = NumberFormat.getCurrencyInstance({
+				currencyCode: false,
+				customCurrencies: {
+					myCurrency: {
+						isoCode: "KRW",
+						decimals: 0
+					}
+				}
+			});
+			return oCurrencyFormatter.format(value, "myCurrency");
+		}
     });
 });
